8a6d8b9c36d77cc2d2aaba4f29f0dc87131b7930
faizudine <mfaizsyafiq2003@gmail.com>
2023-12-06 11:52:28 +0700

    Transaksi berhasisl dibuat

---
+++ application/models/M_Peminjaman.php
@@ -0,0 +1,52 @@
+<?php
+    class M_Peminjaman extends CI_Model
+    {
+        function fetch_all()
+        {
+            $query = $this->db->get('transaksi_peminjaman');
+            return $query->result_array();
+        }
+        function check_data($id)
+        {
+            $this->db->where("id", $id);
+            $query = $this->db->get('transaksi_peminjaman');
+
+            if ($query->row()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        function fetch_single_data($id)
+        {
+            $this->db->where('id',$id);
+            $query = $this->db->get('transaksi_peminjaman');
+            return $query->row();
+        }
+        function insert_api($data)
+        {
+            $this->db->insert('transaksi_peminjaman', $data);
+            if ($this->db->affected_rows() > 0){
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        function update_data($id, $data)
+        {
+            $this->db->where("id", $id);
+            $this->db->update("transaksi_peminjaman", $data);
+        }
+        function delete_data($id)
+        {
+            $this->db->where("id", $id);
+            $this->db->delete("transaksi_peminjaman");
+            if ($this->db->affected_rows() > 0){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+?>
