83a3ed81ff4097bb0a2bb3c26d0208c8929f5a94
HilmiStd <2205040027@student.unimma.ac.id>
2023-12-09 11:57:34 +0700

    done with dashboard: refactor and add jwt auth

--- application/controllers/api/admin/Dashboard.php
+++ application/controllers/api/admin/Dashboard.php
@@ -7,31 +7,55 @@
 
     class Dashboard extends REST_Controller
     {
+
         function __construct()
         {
             parent::__construct();
+            header('Access-Control-Allow-Origin:*');
+            header("Access-Control-Allow-Headers:X-API-KEY,Origin,X-Requested-With,Content-Type,Accept,Access-Control-Request-Method,Authorization");
+            header("Access-Control-Allow-Methods:GET,POST,OPTIONS,PUT,DELETE");
+            $method = $_SERVER['REQUEST_METHOD'];
+            if ($method == "OPTIONS") {
+                die();
+            }
             $this->load->database();
-            // load dashboard model
             $this->load->model('DashboardModel');
-            $this->load->library('form_validation');
+            $this->load->library('jwt');
+        }
+        public function options_get() {
+            header("Access-Control-Allow-Origin: *");
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+            header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+            exit();
         }
 
-        function history_get(){
-            $data = $this->DashboardModel->getCounthistory();
+        function index_get()
+        {
+            if (isset($this->input->request_headers()['Authorization'])){
+                if ($this->jwt->decode($this->input->request_headers()['Authorization'])==false) {
+                    return $this->response(
+                        array(
+                            'kode' => '401',
+                            'pesan' => 'signature tidak sesuai',
+                            'data' => []
+                        ), 401
+                    );
+                }
+            } else{
+                return $this->response(
+                    array(
+                        'kode' => '401',
+                        'pesan' => 'Unauthorized',
+                        'data' => []
+                    ), 401
+                );
+            }
+            $data = array(
+                'user' => $this->DashboardModel->getCountuser(),
+                'buku' => $this->DashboardModel->getCountbuku(),
+                'transaksi' => $data = $this->DashboardModel->getCountTransaksi(),
+                'history' => $this->DashboardModel->getCounthistory()
+            );
             return $this->response($data, 200);
         }
-        function user_get(){
-            $data = $this->DashboardModel->getCountuser();
-            return $this->response($data,200);
-        }
-        function buku_get(){
-            $data = $this->DashboardModel->getCountbuku();
-            return $this->response($data,200);
-        }
-        function transaksi_get(){
-            $data = $this->DashboardModel->getCountTransaksi();
-            return $this->response($data,200);
-        }
     }
-
-?>
